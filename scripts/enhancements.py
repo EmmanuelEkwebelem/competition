32fa72e3a743889df56b07bf09d5de422fe2ad7c
EmmanuelEkwebelem <93545665+EmmanuelEkwebelem@users.noreply.github.com>
2022-12-15 01:30:46 -0500

    Update

--- scripts/enhancements.py
+++ scripts/enhancements.py
@@ -6,6 +6,8 @@ import pgeocode
 from datetime import datetime
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
+
 ### Assessing Dataset ###
 
 
@@ -19,13 +21,6 @@ NoShowData2 = NoShowData
 NoShowData2.replace('', numpy.nan, inplace=True)
 thresh = len(NoShowData2) * .2
 NoShowData2.dropna(thresh = thresh, axis = 1, inplace = True)
-
-### Assessment Results ###
-# There are more rows with missing data than there are columns with missing data.
-# It would be more beneficial to enhance the NoShowData2 (100000 Rows) vs NoShowData1 (3265 Rows). Despite the fact that NoShowData1 has more columns (30) than NoShowData2 (19)
-# Goal: Assess relationship between No Shows and [Age], [Distance], [Weather].
-
-
 
 
 
@@ -44,17 +39,18 @@ DistanceEnhancement = DistanceEnhancement.dropna(axis = 0)
 DistanceEnhancement['patient_zipcode_x'] = DistanceEnhancement['patient_zipcode_x'].astype(str)
 DistanceEnhancement['patient_zipcode_x'] = DistanceEnhancement['patient_zipcode_x'].apply(lambda x: x.zfill(5))
 DistanceEnhancement['patient_zipcode_x'] = DistanceEnhancement['patient_zipcode_x'].str[:5]
-
+# Calculating distance (miles) between Patient and Practice Zipcode using 'pgeocode' API
 def get_distance(x, y):
     usa_zipcodes = pgeocode.GeoDistance('us')
     distance_in_kms = usa_zipcodes.query_postal_code(x.values, y.values)
     return distance_in_kms
 DistanceEnhancement['patient_distance_from_practice (miles)'] = get_distance(DistanceEnhancement['patient_zipcode_x'], DistanceEnhancement['practice_zipcode'])
 DistanceEnhancement['patient_distance_from_practice (miles)'] = DistanceEnhancement['patient_distance_from_practice (miles)'] * 0.621371
+DistanceEnhancement['patient_distance_from_practice (miles)'] = DistanceEnhancement['patient_distance_from_practice (miles)'].round(2)
 # Removing rows with empty values in patient_distance_from_practice (miles) column
 DistanceEnhancement['patient_distance_from_practice (miles)'].replace('', numpy.nan, inplace = True)
 DistanceEnhancement.dropna(subset=['patient_distance_from_practice (miles)'], inplace = True)
-DistanceEnhancement['patient_distance_from_practice (miles)'] = DistanceEnhancement['patient_distance_from_practice (miles)'].round(2)
+# Grouping distance ranges within dataset into a new oclumn 
 DistanceEnhancement.loc[DistanceEnhancement['patient_distance_from_practice (miles)']<5.01, 'patient_distance_from_practice_group'] = 'nearby'
 DistanceEnhancement.loc[DistanceEnhancement['patient_distance_from_practice (miles)'].between(5.01,10), 'patient_distance_from_practice_group'] = 'close'
 DistanceEnhancement.loc[DistanceEnhancement['patient_distance_from_practice (miles)'].between(10.01,30), 'patient_distance_from_practice_group'] = 'far'
@@ -63,7 +59,6 @@ DistanceEnhancement.loc[DistanceEnhancement['patient_distance_from_practice (mil
 # Reordering dataset columns
 NewDistanceEnhancementColumnOrder = ['practice_id', 'practice_zipcode', 'patient_id',
 'patient_zipcode_x', 'patient_distance_from_practice (miles)', 'appointment_yosi_noshow1', 'patient_distance_from_practice_group']
-# Calculating distance between patient zipcode and practice zipcode in miles
 DistanceEnhancement = DistanceEnhancement.reindex(columns = NewDistanceEnhancementColumnOrder)
 # Exporting dataset
 DistanceEnhancement.to_csv('data/EnhancedDatasets/DistanceEnhancement.csv', index = False)
@@ -85,6 +80,8 @@ AgeEnhancement.loc[AgeEnhancement['patient_age']>65, 'patient_age_group'] = 'eld
 # Reordering dataset columns
 NewAgeEnhancementColumnOrder = ['patient_id', 'patient_age', 'appointment_yosi_noshow1', 'patient_age_group']
 AgeEnhancement = AgeEnhancement.reindex(columns = NewAgeEnhancementColumnOrder)
+# Removing rows within the dataset with empty cells
+AgeEnhancement = AgeEnhancement.dropna(axis = 0)
 # Exporting dataset
 AgeEnhancement.to_csv('data/EnhancedDatasets/AgeEnhancement.csv', index = False)
 
@@ -96,21 +93,14 @@ WeatherData1 = pandas.merge(NoShowData1, practiceLocation, on = 'practice_id', h
 WeatherData1 = WeatherData1.dropna(axis = 0)
 WeatherData2 = pandas.merge(NoShowData2, practiceLocation, on = 'practice_id', how = 'inner')
 WeatherData2 = WeatherData2.dropna(axis = 0)
-# Re-ordering dataset columns
-NewWeatherData1ColumnOrder =  ['practice_id', 'practice_zipcode', 'patient_id', 
-'appointment_start_time_groupper','appointment_date', 'weather_icon', 'appointment_yosi_noshow1']
-WeatherData1 = WeatherData1.reindex(columns = NewWeatherData1ColumnOrder)
-NewWeatherData2ColumnOrder = ['practice_id', 'practice_zipcode', 'patient_id', 
-'appointment_start_time_groupper','appointment_date', 'appointment_yosi_noshow1']
-WeatherData2 = WeatherData2.reindex(columns = NewWeatherData2ColumnOrder)
 # Loading Weather Enhancements Datatset for 19128 Zipcode between 10-29-2019 & 12-1-2021
-# Used 19128 Zipcode because it was the most frequent zipcode is WeatherData2 Dataset (11883 Rows)
+# Used 19128 Zipcode because it was the most frequent zipcode in WeatherData2 Dataset (11883 Rows)
 WeatherData_19128 = pandas.read_csv('data/weatherconditions.csv')
 # Renaming columns names to align with previous datasets
 WeatherData_19128.rename(columns = {'zipcode':'practice_zipcode'}, inplace = True)
 WeatherData_19128.rename(columns = {'datetime':'appointment_date'}, inplace = True)
 WeatherData_19128.rename(columns = {'icon':'weather_icon'}, inplace = True)
-# Reformatting column datatypes for merger
+# Reformatting column datatypes befor merging
 WeatherData_19128['practice_zipcode'] = WeatherData_19128['practice_zipcode'].astype(str)
 WeatherData_19128['appointment_date'] = pandas.to_datetime(WeatherData_19128['appointment_date'])
 WeatherData2['appointment_date'] = pandas.to_datetime(WeatherData2['appointment_date'])
@@ -118,20 +108,18 @@ WeatherData2['appointment_date'] = pandas.to_datetime(WeatherData2['appointment_
 EnhancingWeatherData = pandas.merge(WeatherData2, WeatherData_19128, on = ['practice_zipcode', 'appointment_date'], how = 'inner')
 # Combining EnhancingWeatherData with WeatherData1
 WeatherEnhancement = pandas.concat([EnhancingWeatherData, WeatherData1], ignore_index=True, sort=False)
-# Reordering dataset columns
+# Reordering WeatherEnhancement dataset columns
 NewWeatherEnhancementColumnOrder = ['practice_id', 'patient_id', 'appointment_date', 
 'appointment_yosi_noshow1', 'weather_icon']
 WeatherEnhancement = WeatherEnhancement.reindex(columns = NewWeatherEnhancementColumnOrder)
 # Exporting dataset
 WeatherEnhancement.to_csv('data/EnhancedDatasets/WeatherEnhancement.csv', index = False)
 
-# Process Explained: WeatherData1 already has a "Weather Condition Column" So I chose to enhance WeatherData2 and then combine WeatherData1 & WeatherData2 and assess the combined WeatherEnhancement Dataset
+# Process for WeatherEnhancement: WeatherData1 already has a "Weather Condition Column" So I chose to enhance WeatherData2 and then combine WeatherData1 & WeatherData2 
+# and assess the combined WeatherEnhancement Dataset
 
-### I initially seperated Each "_Enhancment.csv" as it made it easier for me to analyze/understand
-### I combined all 3 Enhancement files into a master csv in this script
-
+### Merging Each "_Enhancment.csv" into a master.csv file
 master = pandas.concat([DistanceEnhancement, AgeEnhancement, WeatherEnhancement], ignore_index=True, sort=False)
 master.drop_duplicates(subset=['patient_id'], keep=False)
-
 master.to_csv('data/master.csv', index = False)
-print()
+
